97479162e8680adf9b9f967fd0236bb275d2f174
snowjug <24130500008@reva.edu.in>
2026-05-18 23:15:13 +0530

    feat: implement real Google Auth via @react-oauth/google

--- mimo-website/src/main.tsx
+++ mimo-website/src/main.tsx
@@ -4,11 +4,16 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./styles/index.css";
 import { ThemeProvider } from "./app/components/theme-provider.tsx";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || "";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <App />
-    </ThemeProvider>
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <App />
+      </ThemeProvider>
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
